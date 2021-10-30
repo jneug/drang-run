60d14409a70960ea6fa0650dbb52e20b45c035b6
J. Neugebauer <ngb@helmholtz-bi.de>
2021-10-30 17:12:47 +0200

    Added tests for --def option

--- tests/test_drang_run.py
+++ tests/test_drang_run.py
@@ -55,7 +55,9 @@ def test_two_arg_pos_int(start, stop):
     assert "\n".join(str(x) for x in range(start, stop + 1)) + "\n" == result.output
 
 
-@pytest.mark.parametrize("start,stop", [(-5, 10), (10, -20), (-1, 40), (-99, 100)])
+@pytest.mark.parametrize(
+    "start,stop", [(0, 10), (5, 10), (10, -20), (-1, 40), (-99, 100), (-10, -50)]
+)
 def test_two_arg_mixed_int(start, stop):
     runner = CliRunner()
 
@@ -68,7 +70,10 @@ def test_two_arg_mixed_int(start, stop):
     assert "\n".join(str(x) for x in gen) + "\n" == result.output
 
 
-@pytest.mark.parametrize("start,stop,step", [(1, 10, 1), (1, 20, 2), (100, 40, -1), (99, 100, 2), (99, 100, -2)])
+@pytest.mark.parametrize(
+    "start,stop,step",
+    [(1, 10, 1), (1, 20, 2), (100, 40, -1), (99, 100, 2), (99, 100, -2)],
+)
 def test_three_arg_mixed_int(start, stop, step):
     runner = CliRunner()
 
@@ -78,10 +83,10 @@ def test_three_arg_mixed_int(start, stop, step):
         if step < 0:
             gen = range(start, stop - 1, step)
         else:
-            gen = range(start, stop - 1, -1*step)
+            gen = range(start, stop - 1, -1 * step)
     else:
         if step < 0:
-            gen = range(start, stop + 1, -1*step)
+            gen = range(start, stop + 1, -1 * step)
         else:
             gen = range(start, stop + 1, step)
 
@@ -95,3 +100,89 @@ def test_step_zero(start, stop):
     result = runner.invoke(run, args=[str(start), str(stop), "0"])
     assert result != 0
     assert "Error: Invalid value for '[STEP]'" in result.output
+
+
+@pytest.mark.parametrize(
+    "start,stop", [(0, 10), (5, 10), (10, -20), (-1, 40), (-99, 100), (-10, -50)]
+)
+def test_opt_def(start, stop):
+    runner = CliRunner()
+
+    result = runner.invoke(
+        run, args=[str(start), str(stop), "--def", "a", "{0}+10", "-f", "{0}+10={a}"]
+    )
+    assert result != 0
+
+    if start > stop:
+        gen = range(start, stop - 1, -1)
+    else:
+        gen = range(start, stop + 1)
+    assert "\n".join(f"{x}+10={x+10}" for x in gen) + "\n" == result.output
+
+
+def test_opt_def_akku():
+    runner = CliRunner()
+
+    expected = [
+        6,
+        13,
+        21,
+        30,
+        40,
+        51,
+        63,
+        76,
+        90,
+        97,
+        105,
+        114,
+        124,
+        135,
+        147,
+        160,
+        174,
+        189,
+        197,
+        206,
+        216,
+        227,
+        239,
+        252,
+        266,
+        281,
+        297,
+        306,
+        316,
+        327,
+        339,
+        352,
+        366,
+        381,
+        397,
+        414,
+    ]
+
+    result = runner.invoke(
+        run,
+        args=[
+            "1",
+            "4",
+            "--also",
+            "5",
+            "13",
+            "1",
+            "--def",
+            "a",
+            "{0}",
+            "--def",
+            "sum",
+            "{a}+{1}",
+            "--def",
+            "akk",
+            "{akk}+{sum}",
+            "-f",
+            "{akk}",
+        ],
+    )
+    assert result != 0
+    assert "\n".join(str(x) for x in expected) + "\n" == result.output
